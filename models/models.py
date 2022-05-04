38d131515c098784954811049b51fc5dc0853a7c
Blaucel <104799993+Blaucel@users.noreply.github.com>
2022-05-04 20:34:10 +0200

    Add files via upload

--- models/models.py
+++ models/models.py
@@ -8,15 +8,17 @@ class Fish(models.Model):
     _name = 'ejro2122.fish'
     _description = 'Fish species'
 
-    specie = fields.Char()
-    comname = fields.Char()
-    feeding = fields.Char()
-    weight = fields.Integer(help="Must be expressed in g")  # in grams (g)
-    eggtime = fields.Integer(help="Must be expressed in days (d)") # in days (d)
-    larvaetime = fields.Integer(help="Must be expressed in days (d)")  # in days (d)
-    juventime = fields.Integer(help="Must be expressed in days (d)")  # in days (d)
-    adultime = fields.Integer(help="Must be expressed in days (d)")  # in days (d)
-    lifecycle = fields.Integer(compute="_life_cycle", store=True)  # in days (d)
+    specie = fields.Char(string='Fish specie')
+    comname = fields.Char(string='Common name')
+    fishimg = fields.Image()
+    feeding = fields.Selection(
+        [('omnivorous', 'Omnivorous'), ('carnivorous', 'Carnivorous'), ('herbivorous', 'Herbivorous')], string='Type of feeding', default='none')
+    weight = fields.Integer(string='Average weight (g)', help="Must be expressed in g")  # in grams (g)
+    eggtime = fields.Integer(string='Egg period (d)', help="Must be expressed in days (d)") # in days (d)
+    larvaetime = fields.Integer(string='Larvae period (d)', help="Must be expressed in days (d)")  # in days (d)
+    juventime = fields.Integer(string='Juvenile period (d)', help="Must be expressed in days (d)")  # in days (d)
+    adultime = fields.Integer(string='Adult period (d)', help="Must be expressed in days (d)")  # in days (d)
+    lifecycle = fields.Integer(string='Full life cycle (d)', compute="_life_cycle", store=True)  # in days (d)
     description = fields.Text()
 
     @api.depends('eggtime', 'larvaetime', 'juventime', 'adultime')
@@ -38,19 +40,28 @@ class Pool(models.Model):
     description = fields.Text()
 
 
-# class Farming(models.Model):
-#     _name = 'ejro2122.farm'
-#     _description = 'Farming process'
-#
-#     pool = fields.Char()  # referencia a una piscina
-#     specie = fields.Char()  # referencia a un pez
-#     feeding = fields.Char()  # debe sacar la info del pez
-#     lifecycle = fields.Integer()  # debe sacar la info del pez
-#     transferdate = fields.Datetime('Datetime', default=fields.datetime.now())
-#     fishingdate = fields.Datetime('Datetime', default=fields.datetime.now())  # Hacer el calculo
+class Farm(models.Model):
+    _name = 'ejro2122.farm'
+
+    pool = fields.Char()
+    specie = fields.Char()
+    feeding = fields.Char()
+    lifecycle = fields.Char()
+    transferdate = fields.Char()
+    fishingdate = fields.Char()
 #     oncharge = fields.Many2many('hr.employee', string='Employee')
+
 #
-#     @api.depends('transferdate')
-#     def _fishing_date(self):
-#         for record in self:
-#             record.fishingdate = record.transferdate + timedelta(days=record.lifecycle)
+#
+    # pool = fields.One2many('ejro2122.pool', 'name', string='name')  # referencia a una piscina
+    # specie = fields.Many2one('ejro2122.fish', 'specie', string='specie')  # referencia a un pez
+    # feeding = fields.Many2one('ejro2122.fish', 'feeding', string='feeding')  # debe sacar la info del pez
+    # lifecycle = fields.Many2one('ejro2122.fish', 'lifecycle', string='lifecycle')  # debe sacar la info del pez
+    # transferdate = fields.Datetime('Datetime', default=fields.datetime.now())
+    # fishingdate = fields.Datetime('Datetime', default=fields.datetime.now())  # Hacer el calculo
+    # oncharge = fields.Many2many('hr.employee', string='Employee')
+
+    # @api.depends('lifecycle', 'transferdate')
+    # def _fishing_date(self):
+    #     for record in self:
+    #         record.fishingdate = record.transferdate + timedelta(days=record.lifecycle)
